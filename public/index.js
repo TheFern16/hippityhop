a7517a4c2595069d5124409f1ad1a2c53bd53fca
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-01 15:23:22 -0700

    post new lyrics to the server

--- public/index.js
+++ public/index.js
@@ -1,9 +1,19 @@
 angular.module('lyrics', [])
-  .controller('lyricsCtrl', function($scope, lyricsServices) {
+  .controller('lyricsCtrl', function($scope, lyricsServices, $window) {
     $scope.myLyrics = [];
+
     lyricsServices.getLyrics('', function({data}){
       data.forEach(lyric => $scope.myLyrics.push(lyric.lyric));
     })
+
+    $scope.addLyric = function() {
+      var newLyric = $scope.newLyric;
+      lyricsServices.addLyric(newLyric, function(data) {
+        $window.location.reload();
+      })
+    }
+
+
   }).directive('lyricsList', function() {
     return {
       scope: {
