4d8e2f435945db7e331de69786f182f9f800437a
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-01 15:14:41 -0700

    display lyrics on the page

--- public/index.js
+++ public/index.js
@@ -1,7 +1,24 @@
 angular.module('lyrics', [])
   .controller('lyricsCtrl', function($scope, lyricsServices) {
-    $scope.hello = 'hello world';
+    $scope.myLyrics = [];
     lyricsServices.getLyrics('', function({data}){
-      console.log(data);
+      data.forEach(lyric => $scope.myLyrics.push(lyric.lyric));
     })
+  }).directive('lyricsList', function() {
+    return {
+      scope: {
+        lyrics: '<'
+      },
+      restrict: 'E',
+      controllerAs: 'props',
+      bindToController: true,
+      controller: function() {},
+      template: `
+        <ul>
+          <li ng-repeat="lyric in props.lyrics track by $index">
+            {{ lyric }}
+          </li>
+        </ul>
+      `
+    }
   })
