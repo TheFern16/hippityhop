cf8037359150e20b404078e1ad47942db09291f6
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-01 14:58:58 -0700

    added post request method on lyricservice

--- public/services/services.js
+++ public/services/services.js
@@ -1,5 +1,6 @@
 angular.module('lyrics')
   .service('lyricsServices', function($http, $window) {
+
     this.getLyrics = function(param, callback) {
       $http({
         method: 'GET',
@@ -9,4 +10,18 @@ angular.module('lyrics')
         callback(res);
       })
     }
-  })
+
+    this.addLyric = function(param, callback) {
+      $http({
+        method: 'POST',
+        url: 'http://127.0.0.1:1337/lyrics',
+        headers: { 'Content-Type': 'application/json' },
+        data: {
+          lyric: param
+        }
+      }).then((res) => {
+        callback(res);
+      });
+    }
+
+  });
