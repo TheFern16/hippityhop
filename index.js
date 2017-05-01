8ae3d2b7ae073427db6879432821ba4f39fbc1cd
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-01 12:17:17 -0700

    post a song lyric to the database

--- index.js
+++ index.js
@@ -15,5 +15,22 @@ app.get('/lyrics', function(req, res) {
   Lyrics.find({})
     .exec((err, lyrics) => {
       res.status(200).send(lyrics)
+  });
+});
+
+app.post('/lyrics', function(req, res) {
+  Lyrics.findOne({ lyric: req.body.lyric })
+    .exec((err, lyric) => {
+      if (!lyric) {
+        var newLyric = new Lyrics({
+          lyric: req.body.lyric
+        });
+        newLyric.save((err, newLyric) => {
+          if (err) {
+            res.status(500).send(err);
+          }
+          res.status(201).send(newLyric);
+        });
+      }
     });
-})
+});
