a958f5b4b5767b3d5dd93d8f8489b67039c1d4e8
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-02 08:26:23 -0700

    added deploy link to whitelist

--- public/services/config.js
+++ public/services/config.js
@@ -2,6 +2,7 @@ angular.module('lyrics', [])
 .config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     'self',
-    'https://www.youtube.com/**'
+    'https://www.youtube.com/**',
+    'https://lit-wildwood-58440.herokuapp.com/'
   ]);
 });
